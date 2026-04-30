fcbe17d96552a5ca7fab76f5dbff3b9f85c0d438
98001yash <yashchauhan.gaya@gmail.com>
2026-04-30 08:22:36 +0530

    campaign microservice added in the project

---
+++ campaign-service/src/test/java/com/adbidding/campaign_service/CampaignServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adbidding.campaign_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
