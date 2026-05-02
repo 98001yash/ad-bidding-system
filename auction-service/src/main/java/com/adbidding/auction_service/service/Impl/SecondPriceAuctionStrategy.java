576e964836fb038759aab18662684b7e5c158afa
98001yash <yashchauhan.gaya@gmail.com>
2026-05-02 09:05:10 +0530

    implemented the secondPriceAuctionStrategy

---
+++ auction-service/src/main/java/com/adbidding/auction_service/service/Impl/SecondPriceAuctionStrategy.java
@@ -0,0 +1,35 @@
+package com.adbidding.auction_service.service.Impl;
+
+
+
+import com.adbidding.auction_service.service.AuctionStrategy;
+import com.adbidding.events.BidResponseEvent;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SecondPriceAuctionStrategy implements AuctionStrategy {
+
+    @Override
+    public BidResponseEvent selectWinner(List<BidResponseEvent> bids) {
+        return bids.stream()
+                .max(Comparator.comparingDouble(BidResponseEvent::getBidPrice))
+                .orElse(null);
+    }
+
+    @Override
+    public double calculatePrice(List<BidResponseEvent> bids) {
+
+        List<Double> sorted = bids.stream()
+                .map(BidResponseEvent::getBidPrice)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        if (sorted.size() < 2) {
+            return sorted.get(0); // fallback
+        }
+
+        return sorted.get(1); // second highest
+    }
+}
