4b6221b92fdb9e3dfb0c7eabeef643eca987d7d6
98001yash <yashchauhan.gaya@gmail.com>
2026-05-02 08:18:11 +0530

    kafkaTopics added in the kafka-events module

---
+++ kafka-events/src/main/java/com/adbidding/events/common/KafkaTopics.java
@@ -0,0 +1,10 @@
+package com.adbidding.events.common;
+
+public final class KafkaTopics {
+
+    private KafkaTopics() {}
+
+    public static final String BID_REQUEST = "bid-requests";
+    public static final String BID_RESPONSE = "bid-responses";
+    public static final String AUCTION_RESULT = "auction-results";
+}
