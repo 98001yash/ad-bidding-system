d9f8ef7173ab5b34104ae60a15b04ca89e6d104e
98001yash <yashchauhan.gaya@gmail.com>
2026-05-03 08:39:30 +0530

    added the two methods in the campaign controller

---
+++ campaign-service/src/main/java/com/adbidding/campaign_service/controller/CampaignController.java
@@ -0,0 +1,33 @@
+package com.adbidding.campaign_service.controller;
+
+import com.adbidding.campaign_service.entity.Campaign;
+import com.adbidding.campaign_service.service.CampaignService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/campaigns")
+@RequiredArgsConstructor
+@Slf4j
+public class CampaignController {
+
+    private final CampaignService campaignService;
+
+    // CREATE CAMPAIGN
+    @PostMapping
+    public ResponseEntity<Campaign> createCampaign(@RequestBody Campaign campaign) {
+        log.info("API: Create campaign");
+        return ResponseEntity.ok(campaignService.createCampaign(campaign));
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Campaign> getCampaign(@PathVariable Long id) {
+        return ResponseEntity.ok(campaignService.getCampaignById(id));
+    }
+}
