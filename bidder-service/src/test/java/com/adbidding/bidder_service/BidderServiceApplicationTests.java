5d387cc115091ba8fcc47c8b64598c2843852a9a
98001yash <yashchauhan.gaya@gmail.com>
2026-05-04 08:11:02 +0530

    bidder-service added

---
+++ bidder-service/src/test/java/com/adbidding/bidder_service/BidderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adbidding.bidder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
